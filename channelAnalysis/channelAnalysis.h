a0c79dbf7c73922ed3b2801f578a8e96cf8b8aae
Peng Cheng <cp-cp-cp-cp@163.com>
2017-12-07 14:45:19 +0800

    Initial Commit

---
+++ channelAnalysis/channelAnalysis.h
@@ -0,0 +1,19 @@
+//
+//  channelAnalysis.h
+//  channelAnalysis
+//
+//  Created by Peng Cheng on 07/12/2017.
+//  Copyright © 2017 Peng Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for channelAnalysis.
+FOUNDATION_EXPORT double channelAnalysisVersionNumber;
+
+//! Project version string for channelAnalysis.
+FOUNDATION_EXPORT const unsigned char channelAnalysisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <channelAnalysis/PublicHeader.h>
+
+
